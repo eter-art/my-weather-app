0d0b2fc1030916943d47f630e067e7bd1602aebb
eter-art <77591625+eter-art@users.noreply.github.com>
2021-03-25 22:02:40 +0100

    added forecast to the weather-app

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
-function formatDate (date){
+function formatDate (timestamp){
+let date = new Date (timestamp);
+
+let dayIndex = date.getDay();
+let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ let day = days [dayIndex];
+  return ` ${day} ${formatHours(timestamp)}`;
+ } 
+
+
+ function formatHours(timestamp){
+  let date = new Date (timestamp);
+
 let hours = date.getHours();
 if (hours < 10){
   hours = `0${hours}`;
@@ -7,21 +19,17 @@ let minutes = date.getMinutes();
 if (minutes < 10){
   minutes = `0${minutes}`;
 }
-let dayIndex = date.getDay();
-let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
- let day = days [dayIndex];
-  return ` ${day} ${hours}:${minutes}`;
- } 
-
-
- 
+return `${hours}:${minutes}`;
+ }
 
  function displayWeatherCondition(response){  
    document.querySelector("#city").innerHTML=response.data.name;
    document.querySelector("#temperature").innerHTML=Math.round(response.data.main.temp);
  document.querySelector("#humidity").innerHTML=response.data.main.humidity;
   document.querySelector("#Wind").innerHTML = Math.round(response.data.wind.speed);
+   dateElement.innerHTML = formatDate(response.data.dt  *1000);
   document.querySelector("#description").innerHTML = response.data.weather[0].main;
+ 
    let iconElement = document.querySelector("#icon");
     iconElement.setAttribute(
     "src",
@@ -33,12 +41,39 @@ let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sat
  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   }
  
- 
+ function displayForecast(response){
+ let forecastElement = document.querySelector("#forecast");
+ forecastElement.innerHTML=null;
+let forecast = null;
+   
+for  (let index = 0; index < 6; index++) {
+  let forecast = response.data.list[index];
+     forecastElement.innerHTML += ` 
+  <div class="col-2">
+    <h2>
+    ${formatHours(forecast.dt  *1000)}
+    </h2>
+    <img 
+    src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+    alt=""
+     />
+    <div class="weather-forecast-temperature">
+      <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+           </div>
+        </div>
+       `;
+}
+}
 
  function searchCity(city){
   let apiKey= "c6e720f1cbcd8f1e302c88b67fb9a8fa"; 
  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get (apiUrl) .then(displayWeatherCondition);
+
+apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&&appid=${apiKey}&units=metric`;
+axios.get (apiUrl).then (displayForecast);
+
+
 
   }
 
